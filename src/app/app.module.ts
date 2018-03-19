26c35e1008b44090a9520132f7e678af226dff25
Grimaldi <pablo.david.grimaldi@accenture.com>
2018-03-19 01:17:50 -0300

    pickers - spanish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,71 @@ import { HomeComponent } from './home/home.component';
 import { CampaignService } from './home/campaign.service';
 import { AgendaService } from './agenda.service';
 import { LoginComponent } from './login/login.component';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OwlDateTimeIntl, OWL_DATE_TIME_LOCALE} from 'ng-pick-datetime';
 
+
+// here is the default text string
+export const DefaultIntl = {
+  /** A label for the up second button (used by screen readers).  */
+  upSecondLabel: 'Add a second',
+
+  /** A label for the down second button (used by screen readers).  */
+  downSecondLabel: 'Minus a second',
+
+  /** A label for the up minute button (used by screen readers).  */
+  upMinuteLabel: 'Add a minute',
+
+  /** A label for the down minute button (used by screen readers).  */
+  downMinuteLabel: 'Minus a minute',
+
+  /** A label for the up hour button (used by screen readers).  */
+  upHourLabel: 'Add a hour',
+
+  /** A label for the down hour button (used by screen readers).  */
+  downHourLabel: 'Minus a hour',
+
+  /** A label for the previous month button (used by screen readers). */
+  prevMonthLabel: 'Previous month',
+
+  /** A label for the next month button (used by screen readers). */
+  nextMonthLabel: 'Next month',
+
+  /** A label for the previous year button (used by screen readers). */
+  prevYearLabel: 'Previous year',
+
+  /** A label for the next year button (used by screen readers). */
+  nextYearLabel: 'Next year',
+
+  /** A label for the previous multi-year button (used by screen readers). */
+  prevMultiYearLabel: 'Previous 21 years',
+
+  /** A label for the next multi-year button (used by screen readers). */
+  nextMultiYearLabel: 'Next 21 years',
+
+  /** A label for the 'switch to month view' button (used by screen readers). */
+  switchToMonthViewLabel: 'Change to month view',
+
+  /** A label for the 'switch to year view' button (used by screen readers). */
+  switchToMultiYearViewLabel: 'Choose month and year',
+
+  /** A label for the cancel button */
+  cancelBtnLabel: 'Cancelar',
+
+  /** A label for the set button */
+  setBtnLabel: 'Hecho',
+
+  /** A label for the range 'from' in picker info */
+  rangeFromLabel: 'Desde',
+
+  /** A label for the range 'to' in picker info */
+  rangeToLabel: 'Hasta',
+
+  /** A label for the hour12 button (AM) */
+  hour12AMLabel: 'AM',
+
+  /** A label for the hour12 button (PM) */
+  hour12PMLabel: 'PM',
+};
 
 @NgModule({
   declarations: [
@@ -35,7 +98,9 @@ import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
   ],
   providers: [
     CampaignService,
-    AgendaService],
+    AgendaService,
+    {provide: OwlDateTimeIntl, useValue: DefaultIntl},
+    {provide: OWL_DATE_TIME_LOCALE, useValue: 'es'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
