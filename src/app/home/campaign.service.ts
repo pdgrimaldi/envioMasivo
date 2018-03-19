4c4e2ca179eed428711e58eda242c531e53cb4ac
Grimaldi <pablo.david.grimaldi@accenture.com>
2018-03-19 00:14:00 -0300

    pickers

--- src/app/home/campaign.service.ts
+++ src/app/home/campaign.service.ts
@@ -27,7 +27,7 @@ export class CampaignService {
     if (environment.useJsons) {
       return Observable.of('');
     } else {
-      const formattedDate = model.initDate.toISOString().slice(0, 10) + ' ' + model.initTime.toString().slice(16, 24);
+      const formattedDate = model.initDate.toISOString().slice(0, 10) + ' ' + model.initTime.toString().slice(16, 21);
       params = {
         'text': model.msgToSend,
         'description': model.campaingName,
